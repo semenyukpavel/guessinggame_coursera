19a74b1d61d0211782cbd189ea634e02e26830c8
semenyukpavel <semenyukpavel@googlemail.com>
2022-01-09 02:34:11 +0300

    added game logic

---
+++ README.md
@@ -0,0 +1,3 @@
+Guessing Game for The Unix Workbench course final project
+воскресенье,  9 января 2022 г. 02:33:31 (MSK)
+31
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,32 @@
 #!/usr/bin/env bash
+# File: guessinggame.sh
+
+function makeguess {
+	if ! [[ "$1" =~ ^[0-9]+$ ]]
+	then
+		echo "You did not enter a non-negative number"
+		echo "How many file are in the current directory? Your guess:"
+	elif [[ $1 -lt $2 ]]
+	then
+		echo "Your answer is too low"
+		echo "How many file are in the current directory? Your guess:"
+	elif [[ $1 -gt $2 ]]
+	then
+		echo "Your answer is too high"
+		echo "How many file are in the current directory? Your guess:"
+	else
+		echo "Congratulations, you are right!"
+		flag=true
+		break
+	fi
+}
+
+x=$(ls | wc -l | egrep -o "[0-9+]")
+flag=false
+
+echo "How many file are in the current directory? Your guess:"
+while [[ $flag -eq false ]]
+do
+	read response
+	makeguess "$response" "$x"
+done
